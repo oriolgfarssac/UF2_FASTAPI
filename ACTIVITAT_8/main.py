a5b10ab278e43e514a07e66733567614ddb87359
oriol <oriolgfarssac@gmail.com>
2024-11-17 18:08:39 +0100

    new get method done

--- ACTIVITAT_8/main.py
+++ ACTIVITAT_8/main.py
@@ -1,7 +1,28 @@
+from typing import Optional
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+#@app.get("/items/{item_id}")
+#def read_item(item_id: int):
+#    return {"item_id": item_id}
+
+class Item(BaseModel):
+    id: int
+    name: str
+    description: str
+    price: float
+    is_available: bool
+    discount: Optional[float] = None
+
+items = [
+    {"id": 1, "name": "Item 1", "description": "Desc 1", "price": 10.5, "is_available": True},
+]
+
 @app.get("/items/{item_id}")
 def read_item(item_id: int):
-    return {"item_id": item_id}
+    for item in items:
+        if item["id"] == item_id:
+            return item
+        return "404 Item not found"
